e81fa96bd72807af1e706e5d9f42eb79ba4c0239
Jime <jimedanieli.09@gmail.com>
2022-02-15 17:40:59 -0300

    MyForm is optimized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
+//useCallback es una funcion de memorizacion que lo que hace es recordar la funcion y en el caso de que esta sea la misma va a devolver la misma instancia y no una nueva
 import Title from './components/Title'
 import MyForm from './components/Forms/MyForm'
 import MyList from './components/Lists/MyList'
 
 function App() {
   const [valores, setValores] = useState([])
-  const handleSubmit = (values) => {
-    setValores([
-      ...valores,
+  const handleSubmit =useCallback( (values) => {
+    setValores(data=>([
+      ...data,
       values
-    ])
-  }
+    ]))
+  },[])
 
   return (
     <div>
